8fb85b1aa9f1bd9196ba8534a4d94392c410211d
Ndumiso Mkhasibe <ndmkhjhb025@student.wethinkcode.co.za>
2026-09-09 13:29:22 +0200

    feat: Created the Cognito configuration

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+export const cognitoAuthConfig = {
+  authority:
+    "https://cognito-idp.af-south-1.amazonaws.com/af-south-1_yUoNrREYk",
+
+  client_id: "r69nhg9b3afihfp5u0mug61ic",
+
+  redirect_uri: "http://localhost:8080",
+
+  response_type: "code",
+
+  scope: "openid email",
+};
+
+export const cognitoDomain =
+  "https://af-south-1yuonrreyk.auth.af-south-1.amazoncognito.com";
